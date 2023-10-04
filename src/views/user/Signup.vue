e624218ed5b17a4ef36de2e22feb2edd317b1c06
sieunnnn <wldsmtldsm65@gmail.com>
2023-10-04 21:07:14 +0900

    Feat: 회원가입 유효성 검사, api 연결 (#10)

--- src/views/user/Signup.vue
+++ src/views/user/Signup.vue
@@ -1,9 +1,75 @@
 <script setup>
-import { useRouter } from 'vue-router';
-const router = useRouter();
-const goToLogin = () => {
-  router.push('/auth/login');
-};
+  import { useRouter } from 'vue-router';
+  import { ref } from 'vue';
+  import axios from "axios";
+
+  const router = useRouter();
+  const goToLogin = () => {
+    router.push('/auth/login');
+  };
+
+  // 데이터 바인딩
+  const email = ref('');
+  const userNickname = ref('');
+  const password = ref('');
+  const confirmPassword = ref('');
+  const passwordError = ref(null);
+
+  const isEmailValid = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // 이메일 검증 정규식
+    return emailRegex.test(email);
+  }
+
+  const isPasswordValid = (password) => {
+    const lengthRegex = /^.{8,15}$/;
+    const letterRegex = /[A-Za-z]/;
+    const numberRegex = /\d/;
+    const specialCharRegex = /[!@#$%^&*]/;
+
+    return lengthRegex.test(password) &&
+        letterRegex.test(password) &&
+        numberRegex.test(password) &&
+        specialCharRegex.test(password);
+  }
+
+  const signUp = async () => {
+    if (!isEmailValid(email.value)) {
+      alert('유효한 이메일 주소를 입력해주세요.');
+      return;
+    }
+
+    if (!isPasswordValid(password.value)) {
+      passwordError.value = "* 비밀번호는 8 ~ 15 자리 이하, 영문, 숫자, 특수문자를 조합하여 만들어주세요."
+      return;
+    }
+
+    if (password.value !== confirmPassword.value) {
+      alert("비밀번호가 일치하지 않습니다.");
+      return;
+    }
+
+    try {
+      const response = await axios.post('/auth/signup', {
+        email: email.value,
+        userNickname: userNickname.value,
+        password: password.value,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+
+      if (response.status === 200) {
+        alert('회원가입이 완료되었습니다.');
+        router.push('/auth/login');
+      } else {
+        alert('회원가입에 실패하였습니다.');
+      }
+    } catch (error) {
+      console.error('API 요청 중 오류가 발생했습니다: ', error);
+      alert('오류가 발생했습니다.');
+    }
+  }
 </script>
 
 <template>
@@ -17,13 +83,14 @@ const goToLogin = () => {
       </div>
       <div class="signup_form">
         <form>
-          <input type="text" placeholder="이메일을 입력해주세요."><br>
-          <input type="text" placeholder="닉네임을 입력해주세요."><br>
-          <input type="password" placeholder="비밀번호를 입력해주세요."><br>
-          <input type="password" placeholder="비밀번호를 다시한번 입력해 주세요."><br>
-          <p>* 비밀번호는 8 ~ 15 자리 이하, 영문, 숫자, 특수문자를 조합하여 만들어주세요.</p>
+          <input type="text" v-model="email" placeholder="이메일을 입력해주세요."><br>
+          <input type="text" v-model="userNickname" placeholder="닉네임을 입력해주세요."><br>
+          <input type="password" v-model="password" placeholder="비밀번호를 입력해주세요."><br>
+          <input type="password" v-model="confirmPassword" placeholder="비밀번호를 다시한번 입력해 주세요."><br>
+          <p v-if="passwordError">{{ passwordError }}</p>
+          <p v-else-if="!passwordError"></p>
           <div class="signup_button_container">
-            <button type="submit" style="background: #007AFF;">회원 가입</button>
+            <button @click.prevent="signUp" style="background: #007AFF;">회원 가입</button>
             <button @click.prevent="goToLogin" style="background: #8E8E93;">로그인</button>
           </div>
         </form>
