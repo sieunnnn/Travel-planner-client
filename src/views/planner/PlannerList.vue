8317430c79e74a6e4d0d86abc8f728390697cf5b
sieunnnn <wldsmtldsm65@gmail.com>
2023-10-19 02:26:55 +0900

    Feat: 플래너 리스트 api 연결 (#18)

--- src/views/planner/PlannerList.vue
+++ src/views/planner/PlannerList.vue
@@ -1,5 +1,130 @@
 <script setup>
+import {computed, onMounted, ref} from "vue";
+import {useStore} from "vuex";
+import axios from "axios";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 
+const store = useStore();
+
+// access token, 유저 프로필, userId 꺼내오기
+const accessToken = sessionStorage.getItem("accessToken");
+const userProfileImgUrl = computed(() => store.getters.getUserProfileImgUrl).value;
+const userId = computed(() => store.getters.getUserId).value;
+
+// plannerList api 연결
+const plannerList = ref([]);
+
+const getPlannerList = async () => {
+  try {
+    const response = await axios.get('/planner', {
+      headers: {
+        'Authorization': `${accessToken}`
+      },
+      params: {
+        userId: userId
+      }
+    })
+
+    if (response.status === 200) {
+       plannerList.value = response.data.content;
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+onMounted(getPlannerList);
+
+// 플래너 수정 api 연결
+const editingStates = ref({});
+const toggleEditForm = (plannerId) => {
+  editingStates.value[plannerId] = !editingStates.value[plannerId];
+};
+
+const data = ref({
+  plannerId: null,
+  planTitle: '',
+  isPrivate: false,
+});
+
+const editPlannerTitle = async (planner, formData) => {
+  try {
+    const requestBody = {
+      plannerId: planner.plannerId,
+      planTitle: formData.planTitle,
+      isPrivate: formData.isPrivate,
+    };
+
+    const response = await axios.patch('/planner', requestBody, {
+      headers: {
+        'Authorization': `${accessToken}`
+      },
+    });
+
+    if (response.status === 200) {
+      editingStates.value[planner.plannerId] = !editingStates.value[planner.plannerId];
+      planner.isPrivate = formData.isPrivate;
+      planner.planTitle = formData.planTitle;
+
+      data.isPrivate = false;
+      data.planTitle = '';
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+// 플래너 삭제
+const deletePlanner = async (plannerId) => {
+  try {
+    const requestBody = {
+      plannerId: plannerId,
+    };
+
+    const response = await axios.delete('/planner', {
+      headers: {
+        'Authorization': `${accessToken}`
+      },
+      data: requestBody
+    });
+
+    if (response.status === 200) {
+      plannerList.value = plannerList.value.filter(planner => planner.plannerId !== plannerId);
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+// 플래너 생성 Api 연결
+const createState = ref(false);
+const toggleCreateForm = () => {
+  createState.value = !createState.value;
+};
+
+const createPlanner = async (formData) => {
+  try {
+    const requestBody = {
+      planTitle: formData.planTitle,
+      isPrivate: formData.isPrivate,
+    };
+
+    const response = await axios.post('/planner', requestBody, {
+      headers: {
+        'Authorization': `${accessToken}`
+      },
+    });
+
+    if (response.status === 200) {
+      plannerList.value.unshift(response.data);
+      createState.value = false;
+      data.isPrivate = false;
+      data.planTitle = '';
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
 </script>
 
 <template>
@@ -8,20 +133,28 @@
       <div style="margin: 50px 0 35px 0">
         <router-link :to="`/profile?userId=${userId}`">
           <div class="img-contents">
-            <img v-if="userProfileImgUrl" :src="userProfileImgUrl" width="125"/>
+            <img v-if="userProfileImgUrl" :src="userProfileImgUrl" width="125" style="background-color: white"/>
             <img v-else src="../../assets/images/basic_profile.svg" width="125" style="margin: 5px 0 0 5px; background-color: white"/>
           </div>
         </router-link>
       </div>
     </div>
     <div class="planner_list_content">
-      <div class="planner_container">
+      <div class="planner_create_container">
+        <span v-if="!createState" @click="toggleCreateForm"><font-awesome-icon icon="fa-regular fa-calendar-plus"/> 일정 추가하기</span>
+        <form v-if="createState">
+          <input v-model="data.isPrivate" type="checkbox" style="width: 16px; height: 14px; margin-right: 5px"><label style="font-size: 18px">비공개</label><br>
+          <input v-model="data.planTitle" type="text" class="input">
+          <button @click="createPlanner(data)" type="button" class="blue_button" style="width: 50px; font-size: 17px;">등록</button>
+        </form>
+      </div>
+      <div v-for ="planner in plannerList" :key="planner.plannerId" class="planner_container">
         <div class="planner_container_header">
           <div>
-            <font-awesome-icon icon="fa-solid fa-lock" style="margin-right: 10px"/>
-            <font-awesome-icon icon="fa-solid fa-lock-open" style="margin-right: 10px"/>
-            <font-awesome-icon icon="fa-solid fa-user" />
-            <font-awesome-icon icon="fa-solid fa-users" />
+            <font-awesome-icon v-if="planner.isPrivate" icon="fa-solid fa-lock" style="margin-right: 10px"/>
+            <font-awesome-icon v-else-if="!planner.isPrivate" icon="fa-solid fa-lock-open" style="margin-right: 10px"/>
+            <font-awesome-icon v-if="planner.groupMembers && planner.groupMembers.size > 1" icon="fa-solid fa-users"/>
+            <font-awesome-icon v-else icon="fa-solid fa-user"  />
           </div>
           <div>
             <div class="btn-group dropend">
@@ -29,38 +162,28 @@
                 <font-awesome-icon icon="fa-solid fa-ellipsis-vertical" />
               </button>
               <ul class="dropdown-menu">
-                <li>플래너 제목 수정</li>
-                <li>플래너 삭제</li>
+                <li @click="toggleEditForm(planner.plannerId)">플래너 제목 수정</li>
+                <li @click="deletePlanner(planner.plannerId)">플래너 삭제</li>
               </ul>
             </div>
           </div>
         </div>
         <div>
-          <div class="planner_title"  style="margin: 20px 0 5px 0;">
-            신나는 제주 여행 유후
+          <div v-if="!editingStates[planner.plannerId]" class="planner_title"  style="margin: 20px 0 5px 0;">
+            {{planner.planTitle}}
           </div>
-<!--          <form style="margin: 5px 0 5px 0">-->
-<!--            <input type="checkbox" style="width: 16px; height: 14px; margin-right: 5px"><label style="font-size: 18px">비공개</label><br>-->
-<!--            <input type="text" class="input" style="margin: 2px 10px 0 0">-->
-<!--            <button type="submit" class="blue_button" style="width: 50px; font-size: 16px;">수정</button>-->
-<!--          </form>-->
+          <form v-if="editingStates[planner.plannerId]" style="margin: 5px 0 5px 0">
+            <input v-model="data.isPrivate" type="checkbox" style="width: 16px; height: 14px; margin-right: 5px"><label style="font-size: 18px">비공개</label><br>
+            <input v-model="data.planTitle" type="text" class="input" style="margin: 2px 10px 0 0">
+            <button @click="editPlannerTitle(planner, data)" type="button" class="blue_button" style="width: 50px; font-size: 16px;">수정</button>
+          </form>
         </div>
         <div class="group_member_container">
-          <div class="group_member_img">
-          </div>
-          <div class="group_member_img">
-          </div>
-          <div class="group_member_img">
-          </div>
-          <div class="group_member_img">
-          </div>
-          <div class="group_member_img">
+          <div v-for="(member, index) in planner.groupMembers && planner.groupMembers.slice(0, 5)" :key="index" class="group_member_img">
+            <img v-if="member.profileImageUrl" :src="member.profileImageUrl" width="40"/>
+            <img v-else src="../../assets/images/basic_profile.svg" width="40" style="margin:2px 0 0 2px;"/>
           </div>
         </div>
-      </div>
-      <div class="planner_container">
-      </div>
-      <div class="planner_container">
       </div>
     </div>
   </div>
@@ -87,7 +210,7 @@
  .planner_list_content {
    display: flex;
    flex-direction: row;
-   justify-content: space-between;
+   justify-content: flex-start;
    align-items: flex-start;
    width: 100%;
    height: 100%;
@@ -104,11 +227,35 @@
    border: 1px solid #1E1E1C;
    background: rgba(255, 255, 255, 0.90);
    box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.10);
-   width: 30%;
+   width: 310px;
    height: 190px;
+   margin-left: 10px;
    flex-shrink: 0;
    padding: 15px 20px;
    text-overflow : ellipsis;
+ }
+
+ .planner_create_container {
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+
+   border-radius: 15px;
+   border: 1px solid #1E1E1C;
+   background: rgba(255, 255, 255, 0.60);
+   box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.10);
+
+   width: 310px;
+   height: 190px;
+   margin-left: 10px;
+   flex-shrink: 0;
+   padding:0 20px;
+   text-overflow : ellipsis;
+
+   font-size: 24px;
+   font-style: italic;
+   color: #636366;
  }
 
  .planner_container_header {
@@ -166,15 +313,27 @@
 
  .group_member_img {
    margin: 10px 5px 0 0;
+   padding: 0;
    display: flex;
    flex-direction: row;
    justify-content: center;
    align-items: center;
+
    width: 40px;
    height: 40px;
-   flex-shrink: 0;
+
    border-radius: 40px;
+
    border: 1px solid #1E1E1C;
    box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.10);
+   overflow: hidden;
+ }
+
+ .input {
+   margin: 2px 10px 0 0;
+   width: 200px;
+   height: 27px;
+   font-size: 18px;
+   padding:10px;
  }
 </style>
